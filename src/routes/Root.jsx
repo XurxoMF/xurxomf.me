62756754e3adb6bbc2dfc3087b1e40faa49c8d27
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2022-11-24 18:40:15 +0100

    vaporwave bg

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -4,13 +4,15 @@ import Footer from "../components/footer/Footer";
 
 function Root() {
     return (
-        <div className="w-full overflow-x-hidden bg-xmf-light-white dark:bg-xmf-dark-blue text-black dark:text-white text-lg font-xmf-font">
-            <div className="relative m-auto w-full min-h-screen max-w-screen-xl">
-                <Header />
-                <main className="w-full min-h-[calc(100vh-4rem)] px-8 mb-8">
-                    <Outlet />
-                </main>
-                <Footer />
+        <div id="fondo" className="w-full overflow-x-hidden bg-fixed bg-cover bg-center text-black dark:text-white text-lg font-xmf-font">
+            <div className="w-full min-h-screen backdrop-blur-sm bg-xmf-light-white dark:bg-xmf-dark-blue bg-opacity-80 dark:bg-opacity-80">
+                <div className="relative m-auto w-full min-h-screen max-w-screen-xl">
+                    <Header />
+                    <main className="w-full min-h-[calc(100vh-4rem)] px-8 mb-8">
+                        <Outlet />
+                    </main>
+                    <Footer />
+                </div>
             </div>
         </div>
     );
